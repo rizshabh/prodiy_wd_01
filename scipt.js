95aa31673b7bf7d983d7ce7e8df2854f113e2b86
Rishabh <r786chaurasiya@gmail.com>
2025-05-11 18:32:36 +0530

    first commit

---
+++ scipt.js
@@ -0,0 +1,10 @@
+const navbar = document.getElementById('navbar');
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
